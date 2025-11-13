00fbeeb6584fa18ded4a7d28397461dff5f88f75
vandana <vandana@gmail.com>
2025-11-13 18:27:41 +0000

    Second J

--- src/main/java/com/example/sampleI/SampleIntApplication.java
+++ src/main/java/com/example/sampleI/SampleIntApplication.java
@@ -9,7 +9,6 @@ public class SampleIntApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SampleIntApplication.class, args);
         System.out.println("Hi from Inteelij");
-
 	}
 
 }
