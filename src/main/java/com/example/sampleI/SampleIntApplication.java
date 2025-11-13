c7918df247682761eaad338eef6c61bc13049d0c
vandana <vandana@gmail.com>
2025-11-13 17:52:32 +0000

    First commit

---
+++ src/main/java/com/example/sampleI/SampleIntApplication.java
@@ -0,0 +1,13 @@
+package com.example.sampleI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleIntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleIntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sampleI/SampleIntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampleI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleIntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
